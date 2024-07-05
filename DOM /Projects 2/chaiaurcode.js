d016ba29a2b17595458ce613c4e2902ce549bd87
Zohaib Akhtar <98112123+zohaibakhtar28@users.noreply.github.com>
2024-07-05 12:02:39 +0000

    Modified Projects

--- DOM /Projects 2/chaiaurcode.js
+++ DOM /Projects 2/chaiaurcode.js
@@ -1,20 +1,20 @@
 const form = document.querySelector('form');
 
-
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const height = parseInt(document.querySelector('#height').value);
-  const weight = parseInt(document.querySelector('#weight').value);
-  const result = document.querySelector('#results');
+  const height = parseInt(document.getElementById('height').value);
 
-  if (height === '' || height < 0 || isNaN(height)) {
-    result.innerHTML = `Enter Valid Height ${height}`;
-  } else if (weight === '' || weight < 0 || isNaN(weight)) {
-    weight.innerHTML = `Enter Valid Weight ${weight}`;
-  } else {
-    const bmi = (weight/((height * height)/10000)).toFixed(2)
-    result.innerHTML = `BMI is ${bmi}`;
+  const weight = parseInt(document.getElementById('weight').value);
+
+  const res = document.querySelector('#results');
+
+  if (height < 0 || isNaN(height) || height === '') {
+    res.innerHTML = 'Enter Valid Height';
   }
-
-  //console.log(height)
+  if (weight < 0 || isNaN(weight) || weight === '') {
+    res.innerHTML = 'Enter Valid weight';
+  } else {
+    const bmi = (weight / ((height * height)/ 10000)).toFixed(2);
+    res.innerHTML = `BMI is ${bmi}`;
+  }
 });
--- Events in JS/Color Changer/chaiaurcode.js
+++ Events in JS/Color Changer/chaiaurcode.js
@@ -1,25 +1,29 @@
-const randomColor = function () {
-    const hex = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += hex[Math.floor(Math.random() * 16)];
-    }
-    return color;
+function generateRandomColors() {
+  const hex = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+const defaultColor = document.body.style.backgroundColor;
+let startInterval = '';
+const startChangingColors = function () {
+  const changebgColor = function () {
+    document.body.style.backgroundColor = generateRandomColors();
   };
-  
-  let startInterval;
-  const startChangingColor = function () {
-    const changebgColor = function () {
-      document.body.style.backgroundColor = randomColor();
-    };
-    startInterval = setInterval(changebgColor, 1000);
-  };
-  const stopChangingColor = function () {
-    clearInterval(startInterval);
-    startInterval = '';
-  };
-  
-  document.querySelector('#start').addEventListener('click', startChangingColor);
-  
-  document.querySelector('#stop').addEventListener('click', stopChangingColor);
-  
+
+  startInterval = setInterval(changebgColor, 1000);
+};
+
+const stopChangingColor = function () {
+  clearInterval(startInterval);
+  startInterval = '';
+  document.body.style.backgroundColor = defaultColor;
+};
+
+const start = document.querySelector('#start');
+const stop = document.querySelector('#stop');
+
+start.addEventListener('click', startChangingColors);
+stop.addEventListener('click', stopChangingColor);
